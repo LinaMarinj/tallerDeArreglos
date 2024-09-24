bf9e5bb144ac4db1afb77e7e31de1cb1ed6e3ca5
lina marin <linamarin2011@gmail.com>
2024-09-24 10:20:47 -0500

    Se crea base menú

---
+++ script.js
@@ -0,0 +1,43 @@
+
+let menu;
+
+do {
+
+    menu = parseInt(prompt("Hola, selecciona uno de los ejercicios del listado: \n\n1. \n2. \n3. \n4. \n5. \n6. \n7. \n8. \n 9. \n10."));
+
+
+    switch (menu) {
+        case 1:
+             /* Duplicar cada valor en el arreglo Dado el arreglo [1, 2, 3, 4, 5], usa map para devolver un nuevo arreglo con cada valor duplicado. */
+          
+             let numeros = [1, 2, 3, 4, 5];
+             let numerosDuplicados = numeros.map(function (numero) {
+                
+
+              })
+              alert()
+             
+              
+            break;
+
+        case 2:
+
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+
+        default:
+            break;
+    }
+
+
+
+
+
+} while (menu != 0);
